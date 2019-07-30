0eaf11cad4bd57582f7a14a5520e6cd543eb0ad5
daguimu <dagm@yonyou.com>
2019-07-30 16:16:37 +0800

    添加javadoc annotation

--- src/main/java/com/dagm/devtool/utils/RegUtils.java
+++ src/main/java/com/dagm/devtool/utils/RegUtils.java
@@ -14,27 +14,22 @@ import java.util.regex.Pattern;
  * 字符串正则相关工具包
  *
  * @author: Guimu
- * @created: 2019/07/29
  */
 public class RegUtils {
 
     /**
-     * @Description: 判断字符串是否符合该正则模式, 符合返回true，else 返回false
-     * @Param: [reg, source]
-     * @Return: boolean
-     * @Author: Guimu
-     * @Date: 2018/8/21  下午3:11
+     * @author dagm
+     * @version 0.0.1
+     * @since 1.8
      */
     public static boolean test(String reg, String source) {
         return Pattern.matches(reg, source);
     }
 
     /**
-     * @Description: 获取source源数据中符合指定模式的内容，以List的方式返回
-     * @Param: [reg, source]
-     * @Return: java.util.List<java.lang.String>
-     * @Author: Guimu
-     * @Date: 2018/8/21  下午3:15
+     * @author dagm
+     * @version 0.0.1
+     * @since 1.8
      */
     public static List<String> getPatternContent(String reg, String source) {
         List<String> resultList = new ArrayList<>();
