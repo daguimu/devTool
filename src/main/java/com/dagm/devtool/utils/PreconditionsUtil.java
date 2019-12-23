58b532a18e9e34b35b8a42e32f309014d7607e9f
daguimu <daguimu@maoyan.com>
2019-12-23 23:44:32 +0800

    some thing opm

--- src/main/java/com/dagm/devtool/utils/PreconditionsUtil.java
+++ src/main/java/com/dagm/devtool/utils/PreconditionsUtil.java
@@ -7,12 +7,10 @@ package com.dagm.devtool.utils;
 
 import com.dagm.devtool.common.BaseCode;
 import com.google.common.base.Preconditions;
-import lombok.experimental.UtilityClass;
 
 /**
  * @author: Guimu
  */
-@UtilityClass
 public class PreconditionsUtil {
 
     /**
@@ -21,7 +19,7 @@ public class PreconditionsUtil {
      * @param expression 待校验的表达式
      * @param code 基础错误码
      */
-    public void checkArgument(boolean expression, BaseCode code) {
+    public static void checkArgument(boolean expression, BaseCode code) {
         Preconditions.checkArgument(expression,
             String.format("[错误码: %s, 错误信息: %s]", code.getCode(), code.getMsg()));
     }
