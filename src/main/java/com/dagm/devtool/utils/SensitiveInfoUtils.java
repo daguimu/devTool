1cc117009fdb1129862569c960542d2612e8fccd
daguimu <1712066621@qq.com>
2019-12-24 10:44:11 +0800

    api opm

--- src/main/java/com/dagm/devtool/utils/SensitiveInfoUtils.java
+++ src/main/java/com/dagm/devtool/utils/SensitiveInfoUtils.java
@@ -5,8 +5,9 @@ import org.apache.commons.lang3.StringUtils;
 public class SensitiveInfoUtils {
 
     /**
-     * [中文姓名] 只显示第一个汉字，其他隐藏为2个星号<例：张**>
-     *
+     * [中文姓名] 只显示第一个汉字，其他隐藏为2个星号 例：张**
+     * @return String
+     * @param fullName 原中文姓名
      * @author: Guimu
      * @created: 2019/12/24
      */
@@ -21,7 +22,8 @@ public class SensitiveInfoUtils {
 
     /**
      * 对名字长度进行判断：两个字的脱敏后面[张*];三个字的脱敏中间[张*培];四个字及以上的脱敏前两个以后的[张培**];
-     *
+     * @return String
+     * @param fullName 原中文姓名
      * @author: Guimu
      * @created: 2019/12/24
      */
@@ -55,6 +57,8 @@ public class SensitiveInfoUtils {
     /**
      * [身份证号] 123****12，前面保留3位明文，后面保留2位明文
      *
+     * @return String
+     * @param idcard 原身份证号
      * @author: Guimu
      * @created: 2019/12/24
      */
@@ -69,38 +73,40 @@ public class SensitiveInfoUtils {
     }
 
     /**
-     * [固定电话] 后四位，其他隐藏<例：****1234>
-     *
+     * [固定电话] 后四位，其他隐藏 例：****1234
+     * @param phone 原固定电话
      * @author: Guimu
      * @created: 2019/12/24
      */
-    public static String fixedPhone(String num) {
+    public static String fixedPhone(String phone) {
         String result = "";
-        if (StringUtils.isNotBlank(num)) {
-            result = StringUtils.leftPad(StringUtils.right(num, 4), StringUtils.length(num), "*");
+        if (StringUtils.isNotBlank(phone)) {
+            result = StringUtils.leftPad(StringUtils.right(phone, 4), StringUtils.length(phone), "*");
         }
         return result;
     }
 
     /**
      * [手机号码] 前3位，后4位，其他隐藏<例:123****1234>
-     *
+     * @param phone 原手机号码
+     * @return String
      * @author: Guimu
      * @created: 2019/12/24
      */
-    public static String mobilePhone(String num) {
+    public static String mobilePhone(String phone) {
         String result = "";
-        if (StringUtils.isNotBlank(num)) {
-            result = StringUtils.left(num, 3).concat(StringUtils.removeStart(
-                StringUtils.leftPad(StringUtils.right(num, 4), StringUtils.length(num), "*"),
+        if (StringUtils.isNotBlank(phone)) {
+            result = StringUtils.left(phone, 3).concat(StringUtils.removeStart(
+                StringUtils.leftPad(StringUtils.right(phone, 4), StringUtils.length(phone), "*"),
                 "***"));
         }
         return result;
     }
 
     /**
-     * [地址] 只显示到地区，不显示详细地址；我们要对个人信息增强保护<例：北京****>
-     *
+     * [地址] 只显示到地区，不显示详细地址；我们要对个人信息增强保护 例：北京****
+     * @return String 原地址
+     * @param address
      * @author: Guimu
      * @created: 2019/12/24
      */
@@ -115,8 +121,9 @@ public class SensitiveInfoUtils {
     }
 
     /**
-     * [电子邮箱] 邮箱前缀仅显示第一个字母，前缀其他隐藏，用星号代替，@及后面的地址显示<例:g**@163.com>
-     *
+     * [电子邮箱] 邮箱前缀仅显示第一个字母，前缀其他隐藏，用星号代替，@及后面的地址显示 例:g**@163.com
+     * @param email 原电子邮箱
+     * @return String
      * @author: Guimu
      * @created: 2019/12/24
      */
@@ -135,8 +142,9 @@ public class SensitiveInfoUtils {
     }
 
     /**
-     * [银行卡号] 前四位，后四位，其他用星号隐藏每位1个星号<例:6222600**********1234>
-     *
+     * [银行卡号] 前四位，后四位，其他用星号隐藏每位1个星号 例:6222600**********1234
+     * @return String
+     * @param cardNum 原银行卡号
      * @author: Guimu
      * @created: 2019/12/24
      */
@@ -151,8 +159,9 @@ public class SensitiveInfoUtils {
     }
 
     /**
-     * [公司开户银行联号] 公司开户银行联行号,显示前两位，其他用星号隐藏，每位1个星号<例:12********>
-     *
+     * [公司开户银行联号] 公司开户银行联行号,显示前两位，其他用星号隐藏，每位1个星号 例:12********
+     * @param code 原公司开户银行联号
+     * @return String
      * @author: Guimu
      * @created: 2019/12/24
      */
