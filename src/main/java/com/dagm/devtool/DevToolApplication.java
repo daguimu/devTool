afb768b2a009425d8d53208603df6cf2601d9275
daguimu <1712066621@qq.com>
2020-05-21 18:31:29 +0800

    new

--- src/main/java/com/dagm/devtool/DevToolApplication.java
+++ src/main/java/com/dagm/devtool/DevToolApplication.java
@@ -1,12 +1,8 @@
 package com.dagm.devtool;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DevToolApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DevToolApplication.class, args);
     }
 }
