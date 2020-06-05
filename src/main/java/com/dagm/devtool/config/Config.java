613c4c174b5dfd8f5fbe555497755647f2887d2e
daguimu <1712066621@qq.com>
2020-06-05 11:47:36 +0800

    拓展为starter

--- src/main/java/com/dagm/devtool/config/Config.java
+++ src/main/java/com/dagm/devtool/config/Config.java
@@ -1,24 +1,38 @@
 package com.dagm.devtool.config;
 
+import com.dagm.devtool.advice.GlobalDefultExceptionHandler;
+import com.dagm.devtool.filter.RequestLogFilter;
+import com.dagm.devtool.interceptor.InnerInterceptor;
 import com.dagm.devtool.serializer.FastJson2JsonRedisSerializer;
+import com.dagm.devtool.service.impl.RedisStoreClientImpl;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
-public class RedisConfig extends CachingConfigurerSupport {
+@Import({
+    WebConfiguration.class,
+    RedisStoreClientImpl.class,
+    GlobalDefultExceptionHandler.class,
+    InnerInterceptor.class,
+    RequestLogFilter.class
+})
+@ConditionalOnProperty(name = "spring.redis.enable")
+public class Config extends CachingConfigurerSupport {
 
     @Bean(name = "redisTemplate")
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
