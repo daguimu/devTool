03cc3b62d99bf6c77f89238b592e871ca7dc6454
daguimu <1712066621@qq.com>
2021-05-02 02:33:56 +0800

    bugfix: fixed
    fixed
    
    Signed-off-by: daguimu <1712066621@qq.com>

--- src/main/java/com/dagm/devtool/model/BaseString.java
+++ src/main/java/com/dagm/devtool/model/BaseString.java
@@ -5,10 +5,15 @@
  */
 package com.dagm.devtool.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Guimu
  * @date 2021/05/02
  */
+@Getter
+@Setter
 public class BaseString extends BaseObject {
     private String val;
 
