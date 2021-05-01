2d22cf0c684c90a1e4cf4639399be369f7bd2515
daguimu <1712066621@qq.com>
2021-05-01 17:56:47 +0800

    bugfix: 移除健康检查日志的记录
    移除健康检查日志的记录
    
    Signed-off-by: daguimu <1712066621@qq.com>

--- src/main/java/com/dagm/devtool/filter/RequestLogFilter.java
+++ src/main/java/com/dagm/devtool/filter/RequestLogFilter.java
@@ -5,15 +5,17 @@
  */
 package com.dagm.devtool.filter;
 
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
 
 /**
  * @author Guimu
@@ -24,13 +26,16 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Slf4j
 public class RequestLogFilter extends OncePerRequestFilter {
 
+    private static final String FILTER_STR = "actuator/health";
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest,
-        HttpServletResponse httpServletResponse, FilterChain filterChain)
-        throws ServletException, IOException {
-        log.info("clientIp:[{}]请求访问接口url:[{}]", httpServletRequest.getHeader("X-Real-Ip"),
-            httpServletRequest.getRequestURL());
+                                    HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+        String url = httpServletRequest.getRequestURL().toString();
+        if (StringUtils.isNotEmpty(url) && !url.contains(FILTER_STR)) {
+            log.info("clientIp:[{}]请求访问接口url:[{}]", httpServletRequest.getHeader("X-Real-Ip"), url);
+        }
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
